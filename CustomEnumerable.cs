d3b5222835bab7fceb6b6f40caea4e1fe94478fa
Andrew Smith <als5ev@virginia.edu>
2021-02-19 05:42:32 +0000

    init commit

---
+++ CustomEnumerable.cs
@@ -0,0 +1,58 @@
+// Copyright (c) Microsoft. All rights reserved.
+namespace dotnet_iasyncenumerable
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    class CustomEnumerable<T> : IAsyncEnumerable<string>
+    {
+        CustomEnumerator enumerator;
+
+        public IAsyncEnumerator<string> GetAsyncEnumerator(CancellationToken _)
+        {
+            return this.enumerator;
+        }
+
+        public CustomEnumerable(string input)
+        {
+            this.enumerator = new CustomEnumerator(input);
+        }
+
+        public struct CustomEnumerator : IAsyncEnumerator<string>
+        {
+            string current;
+            string input;
+            int index;
+
+            internal CustomEnumerator(string input)
+            {
+                this.current = null;
+                this.input = input;
+                this.index = 0;
+            }
+
+            public ValueTask DisposeAsync()
+            {
+                return new ValueTask(Task.Run(() => Task.Delay(TimeSpan.FromSeconds(1))));
+            }
+
+            public string Current => this.current;
+
+            public async ValueTask<bool> MoveNextAsync()
+            {
+                await Task.Delay(TimeSpan.FromSeconds(1));
+
+                if (index >= input.Length)
+                {
+                    return false;
+                }
+
+                this.current = input[index].ToString();
+                this.index += 1;
+                return true;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace dotnet_iasyncenumerable
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            IAsyncEnumerable<string> enumerable = new CustomEnumerable<string>("ronald");
+            IAsyncEnumerator<string> enumerator = enumerable.GetAsyncEnumerator();
+            DummyAbstraction dummyAbstraction = new DummyAbstraction(enumerator);
+            await dummyAbstraction.Run();
+        }
+    }
+
+    class DummyAbstraction
+    {
+        IAsyncEnumerator<string> Enumerator;
+        public DummyAbstraction(IAsyncEnumerator<string> enumerator)
+        {
+            this.Enumerator = enumerator;
+        }
+
+        public async Task Run()
+        {
+            bool hasValue = await this.Enumerator.MoveNextAsync();
+
+            if (hasValue)
+            {
+                Console.WriteLine(this.Enumerator.Current);
+            }
+        }
+    }
+}
